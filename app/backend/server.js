4ffe322de9dcf2948db353dd390de8e41089d745
Lassi Heikkinen <lassi@pulu.org>
2015-11-10 16:56:45 +0000

    feature: api methods event, participant, exercice first versions

--- app/backend/server.js
+++ app/backend/server.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var app = express();
+var mysql = require('mysql');
+var myConnection = require('express-myconnection');
 
 // config
 var config = require('./config');
+
+// mysql
+app.use(myConnection(mysql, config.mysql, 'pool'));
 
 // router
 var router = express.Router();
@@ -10,6 +15,70 @@ app.use(router);
 
 // static files
 router.use(express.static(config.basePath + 'build'));
+
+router.use('/api/v1/participant', function(req, res, next) {
+    req.getConnection(function(err, connection) {
+        connection.query('SELECT id, nick, active FROM participant', function(err, results) {
+            if (err) {
+                return next(err);
+            }
+            var out = [];
+            results.forEach(function(row, key) {
+                out.push({
+                    id: row.id,
+                    name: row.nick,
+                    active: row.active === 1 ? true : false
+                });
+            });
+            return res.json(out);
+        });
+    });
+});
+
+router.use('/api/v1/event', function(req, res, next) {
+    req.getConnection(function(err, connection) {
+        connection.query('SELECT * FROM event', function(err, results) {
+            if (err) {
+                return next(err);
+            }
+            var out = [];
+            results.forEach(function(row, key) {
+                out.push({
+                    id: row.id,
+                    name: row.name,
+                    active: row.active === 1 ? true : false,
+                    default_speed: row.default_speed
+                });
+            });
+            return res.json(out);
+        });
+    });
+});
+
+router.use('/api/v1/exercice', function(req, res, next) {
+    req.getConnection(function(err, connection) {
+        connection.query('SELECT id, date, time, nick, laji, raskaus, kommentti, matka, kesto FROM exercice LIMIT 100', function(err, results) {
+            if (err) {
+                return next(err);
+            }
+            var out = [];
+            results.forEach(function(row, key) {
+                out.push({
+                    id: row.id,
+                    created: row.date,
+                    started: row.time,
+                    participant_id: row.nick,
+                    event_id: row.laji,
+                    pace: row.raskaus,
+                    comment: row.kommentti,
+                    distance: row.matka,
+                    duration: row.kesto
+                });
+            });
+            return res.json(out);
+        });
+    });
+});
 
 // route: landing page
 router.use(function(req, res, next) {
