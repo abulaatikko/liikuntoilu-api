dbb0b9de27da2730847e7473d921d199dfed5acd
Lassi Heikkinen <lassi@pulu.org>
2015-11-12 19:23:56 +0000

    feature: testing node-cache cacher

--- app/backend/server.js
+++ app/backend/server.js
@@ -2,6 +2,30 @@
 var express = require('express');
 var app = express();
 
+// cache
+var parseCacheControl = require('parse-cache-control');
+var Cacher = require("cacher")
+var cacher = new Cacher();
+
+cacher.genCacheTtl = function(res, origTtl) {
+    console.log(res.get('Expires'));
+    if (res.get('Cache-Control')) {
+        var cacheControl = parseCacheControl(res.get('Cache-Control'));
+        if (cacheControl['s-maxage']) {
+            return parseInt(cacheControl['s-maxage']);
+        }
+        if (cacheControl['max-age']) {
+            return parseInt(cacheControl['max-age']);
+        }
+    }
+    if (res.get('Expires')) {
+        return (Date.parse(res.get('Expires')) - Date.now()) / 1000;
+    }
+
+    return origTtl;
+};
+app.use(cacher.cache('seconds', 10));
+
 // config
 var config = require('./config');
 
@@ -11,6 +35,7 @@ var repository = require('./repository');
 // router
 var router = express.Router();
 app.use(router);
+
 
 // static files
 router.use(express.static(config.basePath + 'build'));
