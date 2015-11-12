f5da48509188ed234f3da31068381ab767ef4164
Lassi Heikkinen <lassi@pulu.org>
2015-11-12 20:15:53 +0000

    feature: response cache

--- app/backend/server.js
+++ app/backend/server.js
@@ -3,28 +3,9 @@ var express = require('express');
 var app = express();
 
 // cache
-var parseCacheControl = require('parse-cache-control');
-var Cacher = require("cacher")
-var cacher = new Cacher();
-
-cacher.genCacheTtl = function(res, origTtl) {
-    console.log(res.get('Expires'));
-    if (res.get('Cache-Control')) {
-        var cacheControl = parseCacheControl(res.get('Cache-Control'));
-        if (cacheControl['s-maxage']) {
-            return parseInt(cacheControl['s-maxage']);
-        }
-        if (cacheControl['max-age']) {
-            return parseInt(cacheControl['max-age']);
-        }
-    }
-    if (res.get('Expires')) {
-        return (Date.parse(res.get('Expires')) - Date.now()) / 1000;
-    }
-
-    return origTtl;
-};
-app.use(cacher.cache('seconds', 10));
+var cacher = require("cacher")
+var cache = new Cacher();
+app.use(cache.cache('seconds', 10));
 
 // config
 var config = require('./config');
@@ -35,7 +16,6 @@ var repository = require('./repository');
 // router
 var router = express.Router();
 app.use(router);
-
 
 // static files
 router.use(express.static(config.basePath + 'build'));
